8307286605f8af71c3b4fe7505ae648be69c3954
Luis Alcaras <l.alcaras@f5.com>
2026-04-02 19:39:17 -0600

    feat(phisics): basic phisics have been implemented

--- src/player.rs
+++ src/player.rs
@@ -1,14 +1,16 @@
-use bevy::prelude::*;
+use bevy::{math::VectorSpace, prelude::*};
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_player);
-        app.add_systems(Update, animate_sprite);
-        app.add_systems(Update, handle_player_movement);
+        app.add_systems(Update, (animate_sprite, (integrate_phisics, appy_gravity, handle_player_movement).chain()));
     }
 }
+
+#[derive(Component)]
+struct Player;
 
 #[derive(Component)]
 struct AnimationIndices {
@@ -19,8 +21,11 @@ struct AnimationIndices {
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
-#[derive(Component)]
-struct Player;
+#[derive(Component, Deref, DerefMut)]
+struct Aceleration(Vec2);
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
 
 fn animate_sprite(
     time: Res<Time>,
@@ -59,29 +64,49 @@ fn setup_player(
                 index: animation_indices.first,
             },
         ),
-        Transform::from_scale(Vec3::splat(10.0)),
+        Transform::from_scale(Vec3::splat(1.0)),
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
         Player,
+        Velocity(Vec2::ZERO),
+        Aceleration(Vec2::ZERO),
     ));
 }
 
-fn handle_player_movement(input: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<&mut Transform, With<Player>>) {
-    let mut direction = Vec3::ZERO;
-    if input.pressed(KeyCode::Space) {
-        direction.y += 1.0;
-    }
+fn handle_player_movement(input: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<(&mut Aceleration, &mut Velocity), With<Player>>) {
+    for (mut aceleration, mut velocity) in &mut query {
+        **aceleration = Vec2::ZERO;
+        if input.pressed(KeyCode::Space) {
+            aceleration.y += 9.8 * 2.0;
+        }
 
-    if input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
+        if input.pressed(KeyCode::ArrowRight) {
+            aceleration.x += 100.0;
+        }
+        
+        if input.pressed(KeyCode::ArrowLeft) {
+            aceleration.x -= 100.0;
+        }
     
-    if input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
+        **velocity += **aceleration * time.delta_secs();
     }
+}
 
-    for mut transform in &mut query {
-        transform.translation += direction * time.delta_secs() * 100.0;
+fn integrate_phisics(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
+) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation += velocity.extend(0.0) * time.delta_secs();
     }
+}
 
+fn appy_gravity(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
+) {
+    for (mut transform, mut velocity) in &mut query {
+        velocity.y -= 9.8 * time.delta_secs();
+        transform.translation += velocity.extend(0.0) * time.delta_secs();
+    }
 }
